16d4d02104bd096d9920ea167883db856df1e1d6
ErfunBaghaei <66559487+ErfunBaghaei@users.noreply.github.com>
2020-07-02 18:23:09 +0430

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]){
-          System.our.println("yesss ذشذغ");
+          System.out.println("yesss ذشذغ");
 
 System.out.println("by");
 
