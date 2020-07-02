6cfea44f590c5cb098962076b589c610c60ea1b9
apourghani <apourghani@gmail.com>
2020-07-02 18:09:20 +0430

    f

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
-    public static void main(String args[]){
-          System.our.println("yesss ذشذغ");
+    public static void main(String args[]) {
+        System.out.println("yesss ذشذغ");
 
-System.out.println("by");
+        System.out.println("by");
 
     }
 }
