1a2e88557942f3525f9c40a0622f906a1622fa40
apourghani <apourghani@gmail.com>
2020-07-03 18:33:08 +0430

    Amin7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
-
-
     public static void main(String[] args) {
         System.out.println("yesss ذشذغ");
         System.out.println("giiiiiiit");
         System.out.println("salammmmm");
         System.out.println("by");
         System.out.println("hierfun");
-            System.out.println("hierfun");
-
-
+        System.out.println("hierfun");
+        System.out.print("im priinting this at 6:32");
     }
-
 }
