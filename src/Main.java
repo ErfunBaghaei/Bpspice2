c053f89b362a589653aa81aa189225c3493c2ada
apourghani <apourghani@gmail.com>
2020-07-03 16:43:02 +0430

    Amin3 \-

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("giiiiiiit");
         System.out.println("salam");
         System.out.println("by");
+        System.out.println("hierfun");
 
     }
 	}
